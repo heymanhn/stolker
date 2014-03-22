b5bc0c54b974aa24c7fa1db5e34bbcfb21389d7b
Herman Ng <herman@inkling.com>
2014-03-21 19:38:52 -0700

    Add crawler module

---
+++ crawler/crawl.py
@@ -0,0 +1,20 @@
+# Fetch a stock price from Google Finance's website
+import urllib2, locale
+from bs4 import BeautifulSoup
+
+locale.setlocale( locale.LC_ALL, '' )
+GOOGLE_FINANCE_URL = 'http://www.google.com/finance?q='
+
+def fetch_price(ticker_symbol):
+	# We know the fixed URL pattern for Google Finance	
+	url = GOOGLE_FINANCE_URL + ticker_symbol.upper()
+	response = urllib2.urlopen(url)
+	html = response.read()
+
+	html_tree = BeautifulSoup(html, "lxml")
+	price = html_tree.find(class_='pr')
+
+	if price:
+		return locale.atof(price.get_text())
+	else:
+		return 'N/A'
